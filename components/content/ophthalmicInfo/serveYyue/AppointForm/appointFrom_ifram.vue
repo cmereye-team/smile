a89c655b1acfed1495bfc1ab28371d7f25c380c8
jiangzf <xiaojiangzf@163.com>
2023-04-17 18:35:16 +0800

    fix: 修改预约

--- components/content/ophthalmicInfo/serveYyue/AppointForm/appointFrom_ifram.vue
+++ components/content/ophthalmicInfo/serveYyue/AppointForm/appointFrom_ifram.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bookingifram">
+  <div class="page_container bookingifram">
     <iframe width="100%" height="1600" src="https://smilecms.hkcmereye.com/bookingifram"></iframe>
   </div>
 </template>
@@ -20,5 +20,8 @@ export default {
  
 @media screen and (min-width: 768px) { }
 @media screen and (max-width: 768px) { 
+  .bookingifram{
+    padding: 0 !important;
+  }
 }
 </style>
