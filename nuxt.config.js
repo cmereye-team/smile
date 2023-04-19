edd35cacbb43a74a12e4c5b5bbb7d957f9dfbfa3
jiangzf <xiaojiangzf@163.com>
2023-04-19 14:30:02 +0800

    fix: 修改googleLogo

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,7 +45,7 @@ export default {
 
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '/static/favicon.ico' },
       { rel: 'preconnect', href: 'https://fonts.googleapis.com'},
       { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true},
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Baskervville:ital@0;1&family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Noto+Sans+HK:wght@100;300;400;500;700;900&display=swap' },
