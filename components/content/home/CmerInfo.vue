6f46ea0736a06690770fce98afd0141626a387c0
jiangzf <xiaojiangzf@163.com>
2023-04-20 15:48:55 +0800

    fix: 修改类名

--- components/content/home/CmerInfo.vue
+++ components/content/home/CmerInfo.vue
@@ -3,61 +3,61 @@
     <div class="pcShow page_container cmerInfo-in">
       <div class="wow animate__fadeInLeft cmerInfo-in-box" @click="handleInfo('/vision-correction/relex-smile')">
         <img class="cmerInfoTagClick_1" src="https://static.cmereye.com/imgs/2023/04/748ab80a564d55b8.jpg" />
-        <div class="cmerInfoTagClick_1">
-          <h4 v-html="$t('home.cmerInfo[0].h4')"></h4>
-          <span>{{$t('home.cmerInfo[0].span')}}</span>
+        <div>
+          <h4 class="cmerInfoTagClick_1" v-html="$t('home.cmerInfo[0].h4')"></h4>
+          <span class="cmerInfoTagClick_1">{{$t('home.cmerInfo[0].span')}}</span>
         </div>
       </div>
       
       <div class="wow animate__fadeInRight cmerInfo-in-box" @click="handleInfo('/group-profile')">
         <img class="cmerInfoTagClick_2" src="https://static.cmereye.com/imgs/2023/04/69dd6f45ce4fdb12.jpg" />
-        <div class="cmerInfoTagClick_2">
-          <h4 v-html="$t('home.cmerInfo[2].h4')"></h4>
-          <span>{{$t('home.cmerInfo[2].span')}}</span>
+        <div>
+          <h4 class="cmerInfoTagClick_2" v-html="$t('home.cmerInfo[2].h4')"></h4>
+          <span class="cmerInfoTagClick_2">{{$t('home.cmerInfo[2].span')}}</span>
         </div>
       </div>
       <div class="wow animate__fadeInLeft cmerInfo-in-box" @click="handleInfo('/our-medical-team')">
         <img class="cmerInfoTagClick_3" src="https://static.cmereye.com/imgs/2023/04/88bdeb6b94c2375d.jpg" />
-        <div class="cmerInfoTagClick_3">
-          <h4 v-html="$t('home.cmerInfo[1].h4')"></h4>
-          <span>{{$t('home.cmerInfo[1].span')}}</span>
+        <div>
+          <h4 class="cmerInfoTagClick_3" v-html="$t('home.cmerInfo[1].h4')"></h4>
+          <span class="cmerInfoTagClick_3">{{$t('home.cmerInfo[1].span')}}</span>
         </div>
       </div>
       <div class="wow animate__fadeInRight cmerInfo-in-box" @click="handleInfo('/contact-us')">
         <img class="cmerInfoTagClick_4" src="https://static.cmereye.com/imgs/2023/04/e8d6af0160cfc439.jpg" />
-        <div class="cmerInfoTagClick_4">
-          <h4 v-html="$t('home.cmerInfo[3].h4')"></h4>
-          <span>{{$t('home.cmerInfo[3].span')}}</span>
+        <div>
+          <h4 class="cmerInfoTagClick_4" v-html="$t('home.cmerInfo[3].h4')"></h4>
+          <span class="cmerInfoTagClick_4">{{$t('home.cmerInfo[3].span')}}</span>
         </div>
       </div>
     </div>
     <div class="mbShow page_container cmerInfo-in">
       <div class="wow animate__jackInTheBox cmerInfo-in-box" @click="handleInfo('/vision-correction/relex-smile')">
-        <img src="https://static.cmereye.com/imgs/2023/04/1b9fffce0077e770.jpg" />
-        <div class="cmerInfoTagClick">
-          <h4 v-html="$t('home.cmerInfo[0].h4')"></h4>
-          <span>{{$t('home.cmerInfo[0].span')}}</span>
+        <img class="cmerInfoTagClick_1" src="https://static.cmereye.com/imgs/2023/04/1b9fffce0077e770.jpg" />
+        <div>
+          <h4 class="cmerInfoTagClick_1" v-html="$t('home.cmerInfo[0].h4')"></h4>
+          <span class="cmerInfoTagClick_1">{{$t('home.cmerInfo[0].span')}}</span>
         </div>
       </div>
       <div class="wow animate__jackInTheBox cmerInfo-in-box" @click="handleInfo('/group-profile')">
-        <img src="https://static.cmereye.com/imgs/2023/04/60d92825498a1a12.jpg" />
+        <img class="cmerInfoTagClick_2" src="https://static.cmereye.com/imgs/2023/04/60d92825498a1a12.jpg" />
         <div>
-          <h4 v-html="$t('home.cmerInfo[2].h4')"></h4>
-          <span>{{$t('home.cmerInfo[2].span')}}</span>
+          <h4 class="cmerInfoTagClick_2" v-html="$t('home.cmerInfo[2].h4')"></h4>
+          <span class="cmerInfoTagClick_2">{{$t('home.cmerInfo[2].span')}}</span>
         </div>
       </div>
       <div class="wow animate__jackInTheBox cmerInfo-in-box" @click="handleInfo('/our-medical-team')">
-        <img class="cmerInfoTagClick" src="https://static.cmereye.com/imgs/2023/04/3caa4f0732ba738c.jpg" />
-        <div class="cmerInfoTagClick">
-          <h4 v-html="$t('home.cmerInfo[1].h4')"></h4>
-          <span>{{$t('home.cmerInfo[1].span')}}</span>
+        <img class="cmerInfoTagClick_3" src="https://static.cmereye.com/imgs/2023/04/3caa4f0732ba738c.jpg" />
+        <div>
+          <h4 class="cmerInfoTagClick_3" v-html="$t('home.cmerInfo[1].h4')"></h4>
+          <span class="cmerInfoTagClick_3">{{$t('home.cmerInfo[1].span')}}</span>
         </div>
       </div>
       <div class="wow animate__jackInTheBox cmerInfo-in-box" @click="handleInfo('/contact-us')">
-        <img src="https://static.cmereye.com/imgs/2023/04/285b92be603d3f08.jpg" />
-        <div class="cmerInfoTagClick">
-          <h4 v-html="$t('home.cmerInfo[3].h4')"></h4>
-          <span>{{$t('home.cmerInfo[3].span')}}</span>
+        <img class="cmerInfoTagClick_4" src="https://static.cmereye.com/imgs/2023/04/285b92be603d3f08.jpg" />
+        <div>
+          <h4 class="cmerInfoTagClick_4" v-html="$t('home.cmerInfo[3].h4')"></h4>
+          <span class="cmerInfoTagClick_4">{{$t('home.cmerInfo[3].span')}}</span>
         </div>
       </div>
     </div>
