1c7a91cc9e2be2a0a31c4e7871755c0ad5a3fcdd
jiangzf <xiaojiangzf@163.com>
2023-05-09 08:56:31 +0800

    fix: 修改meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,8 +41,8 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0,user-scalable=no' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
-
+      { name: 'format-detection', content: 'telephone=no' },
+      { name: 'google-site-verification', content: '9eEA_1KUjhpF1Is-gEyDoj-rqmZWl4qlc0qjkPZDoIM' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/static/favicon.ico' },
