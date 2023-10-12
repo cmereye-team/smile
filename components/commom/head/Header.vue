0c18a7146b3930545acd2fff5ad91577136bbc6c
qiwu96 <39629631+9777717@users.noreply.github.com>
2023-10-12 01:53:30 -0500

    Update Header.vue
    临时修改

--- components/commom/head/Header.vue
+++ components/commom/head/Header.vue
@@ -250,7 +250,7 @@
           </button>
         </a>
       </div> -->
-      <div class="contact contact_icl-r">
+     <!-- <div class="contact contact_icl-r">
         <a :href="localePath('/icl-r')">
           <button class="link_more shadow-lg">
             <div class="flex items-center justify-evenly">
@@ -261,7 +261,7 @@
             </div>
           </button>
         </a>
-      </div>
+      </div> -->
       <div class="contact contact_divit">
         <a :href="localePath('/divit')">
           <button class="link_more shadow-lg">
