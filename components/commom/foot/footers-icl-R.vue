61b8d56ba297b57b5fb39c5dcdc78c8fbe8a3af0
9777717 <zbb1064212882@126.com>
2023-10-13 15:34:18 +0800

    fix: 新增移动端活动入口

--- components/commom/foot/footers-icl-R.vue
+++ components/commom/foot/footers-icl-R.vue
@@ -2,16 +2,17 @@
   <div class="footerBox">
     <div class="online footer mbShow">
       <div class="foot">
-        <div class="box" v-for="item in footerLists" :key="item.title" @click="handleFoot(item)">
-          <img
-            :src="item.img"
-            alt=""
-          />
-          <p>{{item.title}}</p>
+        <div
+          class="box"
+          v-for="item in footerLists"
+          :key="item.title"
+          @click="handleFoot(item)"
+        >
+          <img :src="item.img" alt="" />
+          <p>{{ item.title }}</p>
         </div>
       </div>
-      <!-- 以下 icl 活动弹窗 -->
-      <!-- <div class="footer-coupon footer_coupon_mb">
+      <div class="footer-coupon footer_coupon_mb">
         <a href="/icl-r">
           <div>
             <svg
@@ -103,7 +104,7 @@
             <span>ICL<br />星級矯視講座</span>
           </div>
         </a>
-      </div> -->
+      </div>
       <div class="footer-coupon">
         <a
           :href="'https://api.whatsapp.com/send/?phone=85260610511&text=%E6%83%B3%E4%BA%86%E8%A7%A3(W-01)SMILE%E5%BE%AE%E7%AC%91%E7%9F%AF%E8%A6%96%E6%B6%88%E8%B2%BB%E5%8D%B7%E6%94%B6%E8%B2%BB%E8%A9%B3%E6%83%85'"
@@ -122,53 +123,51 @@
 export default {
   data() {
     return {
-      footerLists:[
+      footerLists: [
         {
-          type: 'tel',
-          link: '',
-          img:'https://static.cmereye.com/imgs/2023/03/4f2e7a260618edb4.png',
-          title: this.$t('home.footer.title1')
+          type: "tel",
+          link: "",
+          img: "https://static.cmereye.com/imgs/2023/03/4f2e7a260618edb4.png",
+          title: this.$t("home.footer.title1"),
         },
         {
-          type: 'href',
-          link:'https://mqj.zoosnet.net/LR/Chatpre.aspx?id=MQJ40126824&lng=big5&oname=CS_Katherine&r=&rf1=https%3A//smile.hkcmereye&rf2=.com/&p=https%3A//smile.hkcmereye.com/&cid=26ad3fcc05fa407b81167bda7abd1c92&sid=26ad3fcc05fa407b81167bda7abd1c92',
-          img:'https://static.cmereye.com/imgs/2023/03/935a17da94382a86.png',
-          title: this.$t('home.footer.title2')
+          type: "href",
+          link: "https://mqj.zoosnet.net/LR/Chatpre.aspx?id=MQJ40126824&lng=big5&oname=CS_Katherine&r=&rf1=https%3A//smile.hkcmereye&rf2=.com/&p=https%3A//smile.hkcmereye.com/&cid=26ad3fcc05fa407b81167bda7abd1c92&sid=26ad3fcc05fa407b81167bda7abd1c92",
+          img: "https://static.cmereye.com/imgs/2023/03/935a17da94382a86.png",
+          title: this.$t("home.footer.title2"),
         },
         {
-          type: 'href',
-          link:'https://wa.me/85260610511',
-          img:'https://static.cmereye.com/imgs/2023/03/e011cddb240290b9.png',
-          title: 'Whatsapp'
+          type: "href",
+          link: "https://wa.me/85260610511",
+          img: "https://static.cmereye.com/imgs/2023/03/e011cddb240290b9.png",
+          title: "Whatsapp",
         },
         {
-          type: 'link',
-          link:'/booking',
-          img:'https://static.cmereye.com/imgs/2023/03/aa60fde6a44f6099.png',
-          title: this.$t('home.footer.title3')
-        }
-      ]
+          type: "link",
+          link: "/booking",
+          img: "https://static.cmereye.com/imgs/2023/03/aa60fde6a44f6099.png",
+          title: this.$t("home.footer.title3"),
+        },
+      ],
     };
   },
-  mounted() {
-
-  },
+  mounted() {},
   created() {},
   methods: {
-    handleFoot(data){
+    handleFoot(data) {
       // console.log('点击的foot内容: ---->',data)
-      if(data.type === 'link'){
+      if (data.type === "link") {
         this.$router.push({
           path: data.link,
         });
-      }else if(data.type === 'href'){
-        window.open(data.link)
-      }else if(data.type === 'tel'){
-        window.location.href = 'tel:60610511'
+      } else if (data.type === "href") {
+        window.open(data.link);
+      } else if (data.type === "tel") {
+        window.location.href = "tel:60610511";
       }
-    }
+    },
   },
-  watch: {}
+  watch: {},
 };
 </script>
 <style lang="scss" scoped>
@@ -258,44 +257,43 @@ export default {
     color: #fff;
     border-radius: 18px 18px 0 0;
   }
-  .footer-coupon{
+  .footer-coupon {
     width: 80px;
     height: 80px;
     position: absolute;
     bottom: 220px;
     right: 15px;
-    background: #4570B6;
+    background: #4570b6;
     z-index: 9990;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
     border-radius: 50%;
-    div{
+    div {
       flex: 1;
       display: flex;
       flex-direction: column;
       align-items: center;
       justify-content: center;
-      img{
+      img {
         width: 25px;
         // height: 25px;
       }
-      span{
+      span {
         display: block;
         width: 100%;
         text-align: center;
-        color:#fff;
+        color: #fff;
         font-style: normal;
         font-weight: 400;
         font-size: 10px;
         line-height: 17px;
       }
     }
-
   }
-  .footer_coupon_mb{
-    background: #98CEC3;
+  .footer_coupon_mb {
+    background: #98cec3;
     top: -100%;
   }
 }
