1a14e52e20baf648d107169cb56eb049faefa98a
9777717 <zbb1064212882@126.com>
2023-11-23 13:07:35 +0800

    fix: 下架en版本

--- plugins/i18n.js
+++ plugins/i18n.js
@@ -1,14 +1,16 @@
 
 import cn from '../lang/zhCN.js' //简体
 import hk from '../lang/zhHK.js' //繁体
-import en from '../lang/usEn.js' //繁体
+// import en from '../lang/usEn.js' //en
 export default {
     // strategy: 'prefix_except_default',
     defaultLocale: 'hk',
     // 根据项目情况，酌情配置
-    locales: ['cn', 'hk', 'en'],
+    // locales: ['cn', 'hk', 'en'],
+    locales: ['cn', 'hk'],
     vueI18n: {
         fallbackLocale: 'hk',
-        messages: { cn, hk, en }
+        // messages: { cn, hk, en }
+        messages: { cn, hk }
     }
 }
