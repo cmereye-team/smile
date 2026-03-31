c24d0bcc38e04aad3d2dabd1c29d69cd6000f0f7
祥友 <cmerhksz@gmail.com>
2026-03-31 18:01:04 +0800

    添加字体、修改文案等

--- nuxt.config.js
+++ nuxt.config.js
@@ -71,7 +71,7 @@ export default {
       },
       {
         rel: "stylesheet",
-        href: "https://fonts.googleapis.com/css2?family=Baskervville:ital@0;1&family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Noto+Sans+HK:wght@100;300;400;500;700;900&display=swap",
+        href: "https://fonts.googleapis.com/css2?family=Baskervville:ital@0;1&family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Noto+Sans+HK:wght@100;300;400;500;700;900&family=Noto+Sans+TC:wght@100..900&display=swap",
       },
       {
         rel: "stylesheet",
